33ec3a4e9add057d88ae5395e3bf3ec4e4e6343b
paralicm <marek.paralic@winpos.com>
2017-12-18 14:00:11 +0100

    Add project files.

---
+++ edX.DataApp.CoreConsole/ContosoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace edX.DataApp.CoreConsole
+{
+    public class ContosoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLOCALDB;Initial Catalog=ContosoDB;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
---
+++ edX.DataApp.CoreConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace edX.DataApp.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            Console.WriteLine("Application has completed execution. Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        static async Task RunAsync()
+        {
+            using (ContosoContext context = new ContosoContext())
+            {
+                var creator = context.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                await creator.ExistsAsync();
+                Console.WriteLine("Connection Successful");
+            }
+        }
+    }
+}
